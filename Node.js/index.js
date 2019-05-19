e5a7b59d31f50c7c51a80b8e7e4b897c7724cc93
Ivan <stankovicivan86@hotmail.com>
2019-05-19 21:05:07 +0200

    update node edit+delete

--- Node.js/index.js
+++ Node.js/index.js
@@ -8,43 +8,40 @@ app.use(myParser.urlencoded({ extended: true }));
 const session = require("express-session");
 app.use(session({ secret: "test" }));
 
-app.listen(3000);
-
+app.listen(8000);
 
 app.post("/register", (req, res, next) => {
-    var firstname = req.body.firstname;
-    var lastname = req.body.lastname;
-    var email = req.body.email;
-    var birthdate = req.body.birthdate;
-    var telephone = req.body.telephone;
-    var country = req.body.country;
-    var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var birthdate = req.body.birthdate;
+  var telephone = req.body.telephone;
+  var country = req.body.country;
+  var password = req.body.password;
 
-    let user = new User({
-        firstname:firstname,
-        lastname:lastname,
-        email: email,
-        birthdate: birthdate,
-        telephone: telephone,
-        country: country,
-        password: password
-    });
+  let user = new User({
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    birthdate: birthdate,
+    telephone: telephone,
+    country: country,
+    password: password
+  });
 
-    console.log(user)
+  console.log(user);
 
-    user.save(function(err){
-        if(err){
-            return next(err);
-        }
-        res.send("User saved");
-    })
-
+  user.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("User saved");
+  });
 });
 
-app.get("/", (req, res) =>{
-    res.send("Hello");
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 // app.post("/login", (req, res) => {
 //     var email = req.body.emailLogin;
@@ -58,45 +55,59 @@ app.get("/", (req, res) =>{
 // })
 
 app.post("/addProduct", (req, res, next) => {
-        var productName = req.body.productName;
-        var productDescription = req.body.productDescription;
-        var productType = req.body.productType;
-        var purchaseDate = req.body.purchaseDate;
-        var price = req.body.price;
-        var userEmail = req.session.email;
+  var productName = req.body.productName;
+  var productDescription = req.body.productDescription;
+  var productType = req.body.productType;
+  var purchaseDate = req.body.purchaseDate;
+  var price = req.body.price;
+  var userEmail = req.session.email;
 
-        let product = new Product({
-            productName:productName,
-            productDescription:productDescription,
-            productType:productType,
-            purchaseDate:purchaseDate,
-            price: price,
-            userEmail: userEmail
-        });
+  let product = new Product({
+    productName: productName,
+    productDescription: productDescription,
+    productType: productType,
+    purchaseDate: purchaseDate,
+    price: price,
+    userEmail: userEmail
+  });
 
-        product.save(function(err) {
-            if(err){
-                return next(err);
-            }
-            res.send("Product Added");
-        })
+  product.save(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("Product Added");
+  });
 
-        //send response to FE
-  
+  //send response to FE
 });
 
-app.get("/addProduct", (req, res)=>{
-    res.send("New Product");
-})
+app.get("/addProduct", (req, res) => {
+  res.send("New Product");
+});
 
-app.get("/products", (req, res, next)=> {
-    Product.find({}, function(err, products) {
-        if(err) {
-            return next(err);
-        }
-        res.send(products);
-    })
-})
+app.get("/products", (req, res, next) => {
+  Product.find({}, function(err, products) {
+    if (err) {
+      return next(err);
+    }
+    res.send(products);
+  });
+});
 
+app.put("/products/:id", (req, res, next) => {
+  Product.findByIdAndUpdate(req.params.id, req.body, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("Product updated.");
+  });
+});
 
-
+app.delete("/products/:id", (req, res, next) => {
+  Product.deleteOne(req.params._id, function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("Product deleted.");
+  });
+});
